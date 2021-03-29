1987cd33d719549502d7392dd2cca1e5b288b903
Josep V. Monjo <jvmonjo@gmail.com>
2021-03-29 19:39:23 +0200

    ADDED: seeds

---
+++ seeds/default/1617039415303_languages_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.languages (code, name) VALUES ('ca', 'català/valencià');
+INSERT INTO public.languages (code, name) VALUES ('es', 'español');
---
+++ seeds/default/1617039503045_article_statuses_seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.article_statuses (status) VALUES ('DRAFT');
+INSERT INTO public.article_statuses (status) VALUES ('PUBLISHED');
+INSERT INTO public.article_statuses (status) VALUES ('DELETED');
