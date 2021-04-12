09cea01c9a463709c680cf89bb3740ec9f6e5e9d
Josep V. Monjo <jvmonjo@gmail.com>
2021-04-12 12:21:08 +0200

    REMOVED: DELETED STATE

--- seeds/default/1617039503045_article_statuses_seed.sql
+++ seeds/default/1617039503045_article_statuses_seed.sql
@@ -1,3 +1,2 @@
 INSERT INTO public.article_statuses (status) VALUES ('DRAFT');
 INSERT INTO public.article_statuses (status) VALUES ('PUBLISHED');
-INSERT INTO public.article_statuses (status) VALUES ('DELETED');
